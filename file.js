3016a6f6ac582a6c5f6484d524a29f66ed668bbb
Katarzyna Michalak <KaMich@MacBook-Air-Ka-Mich.local>
2017-05-29 22:04:24 +0200

    IF

--- file.js
+++ file.js
@@ -1,4 +1,10 @@
-var a=2,
-	h=4,
-	triangleArea=a*h/2;
-console.log('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
+var value = (a * a) + (2 * a * b) - (b * b)
+console.log(total)
+if (value <0) {
+	console.log(wynik ujemny)
+} else if {
+	console.log(wynik dodatni)
+} else (value=0){
+	console.log(wynik rowny zero)
+}
+
